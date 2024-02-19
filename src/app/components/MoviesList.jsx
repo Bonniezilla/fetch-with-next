dade2b4b3ec1ff48db452217a845b94112d5d486
Bonniezilla <bonnieevil86@gmail.com>
2024-02-19 20:25:01 -0300

    Adding dinamic route of the movies page

--- src/app/components/MoviesList.jsx
+++ src/app/components/MoviesList.jsx
@@ -4,10 +4,17 @@ import Link from "next/link";
 import getMovies from "../../../util/getMovies";
 
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
-export default async function MoviesList({ delay, shouldError }) {
+export default async function MoviesList({ delay, shouldError, page }) {
+    const router = useRouter();
+
+    const handleReload = () => {
+        router.refresh();
+    }
+
     try {
-        const moviesFetch = await getMovies(delay, shouldError);
+        const moviesFetch = await getMovies(delay, shouldError, page);
         const moviesList = moviesFetch.results;
 
         return (
@@ -21,22 +28,21 @@ export default async function MoviesList({ delay, shouldError }) {
                             <Image
                                 width={800}
                                 height={800}
-                                src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+                                src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
                             />
                         </div>
                     )
                 })}
             </div>
-
         )
     } catch (error) {
         console.log(error)
         return (
             <div className="flex flex-col items-center justify-center h-screen w-screen gap-4">
                 <h1 className="text-white text-3xl">Ops! Erro encontrado!</h1>
-                <Link href="/moviesError" className="flex items-center justify-center
+                <button onClick={() => handleReload()} className="flex items-center justify-center
                 p-4 h-12 bg-blue-400 font-bold rounded-md shadow-white-50 shadow-md
-                hover:bg-gray-500 hover:text-slate-300 duration-300">Tentar novamente</Link>
+                hover:bg-gray-500 hover:text-slate-300 duration-300">Tentar novamente</button>
                 <Link href="/" className="flex items-center justify-center
                 p-4 h-12 bg-red-400 font-bold rounded-md shadow-white-50 shadow-md
                 hover:bg-gray-500 hover:text-slate-300 duration-300">Voltar para o inicio</Link>
--- util/getMovies.jsx
+++ util/getMovies.jsx
@@ -2,7 +2,7 @@ export const delay = (ms) => {
     return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-export default async function getMovies(time, shouldError) {
+export default async function getMovies(time, shouldError, page) {
     await delay(time);
     const options = {
         method: 'GET',
@@ -11,7 +11,7 @@ export default async function getMovies(time, shouldError) {
           Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzZGZhOGI5NjBlYTI2MjBiZTZmOTY1YmVmYjk3NDVjZCIsInN1YiI6IjY0Zjg4ZTU4OGUyMGM1MGNkNjJhNTYzNyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.JKyhrivA_iMaLiF8UxnX7Go7Frbo0XnpSHRLciSENvQ'
         }
       };
-    const res = await fetch(`https://api.themoviedb.org/3/movie/popular?language=en-US&page=1`, options)
+    const res = await fetch(`https://api.themoviedb.org/3/movie/popular?language=pt-BR&page=${page}`, options)
     if(!res.ok || shouldError) {
         throw new Error(`Ocorreu um erro: ${res.status}`)
     }
